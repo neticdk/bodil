39081079b63ea45e735da6552edc5f8334eb1d34
Kim Nørgaard <kn@netic.dk>
2015-07-27 21:56:36 +0200

    add sync script

---
+++ sync_coreos_images.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+syncit() {
+  channel=$1
+  version=${2:-current}
+
+  wget --cut-dirs=1 -nH --quiet -A coreos_production_image*,coreos_production_pxe* -m http://${channel}.release.core-os.net/amd64-usr/${version}
+}
+
+cd $(dirname $0)
+
+(
+  cd bodil/static/images/coreos/stable
+  syncit stable
+)
+
+(
+  cd bodil/static/images/coreos/beta
+  syncit beta
+)
+
+(
+  cd bodil/static/images/coreos/alpha
+  syncit alpha
+)
+
